ecb28e2556d98ac91741df78a4c699f43d8be629
aanvarov <anvarov2295@gmail.com>
2022-02-23 15:06:48 +0500

    cart and food models added

---
+++ src/models/cart.model.ts
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from 'mongoose';
+import { FoodDocument } from './food.model';
+import { UserDocument } from './user.model';
+import { RestaurantDocument } from './restaurant.model';
+
+enum DiscountType {
+  PERCENTAGE = 'percentage',
+  NUMBER = 'number',
+}
+
+export interface CartItemDocument extends mongoose.Document {
+  food: FoodDocument['_id'];
+  quantity: number;
+  total: number;
+  size: string;
+}
+
+export interface CartDocument extends mongoose.Document {
+  items: CartItemDocument[];
+  total: number;
+  coupon: string;
+  discount: number;
+  discountType: DiscountType;
+  tax: number;
+  totalWithTax: number;
+  deliveryFee: number;
+  customer: UserDocument['_id'];
+  restaurant: RestaurantDocument['_id'];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const CartSchema = new Schema(
+  {
+    items: [
+      {
+        food: { type: Schema.Types.ObjectId, ref: 'Food' },
+        quantity: { type: Number, default: 1 },
+        total: { type: Number, default: 0 },
+        size: { type: String },
+      },
+    ],
+    total: { type: Number, default: 0 },
+    coupon: { type: String, default: '' },
+    discount: { type: Number, default: 0 },
+    discountType: { type: String, enum: Object.values(DiscountType), default: DiscountType.NUMBER },
+    tax: { type: Number, default: 0 },
+    totalWithTax: { type: Number, default: 0 },
+    deliveryFee: { type: Number, default: 0 },
+    customer: { type: Schema.Types.ObjectId, ref: 'User' },
+    restaurant: { type: Schema.Types.ObjectId, ref: 'Restaurant' },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Cart = mongoose.model<CartDocument>('Cart', CartSchema);
+
+export default Cart;
---
+++ src/models/food.model.ts
@@ -0,0 +1,57 @@
+import mongoose, { Schema, Types } from 'mongoose';
+import { CategoryDocument } from './category.model';
+
+enum Status {
+  IN_STOCK = 'instock',
+  OUT_OF_STOCK = 'outofstock',
+}
+
+export interface FoodDocument extends mongoose.Document {
+  name: string;
+  price: number;
+  img: string;
+  rating: number;
+  description: string;
+  size: [string];
+  status: Status;
+  category: CategoryDocument['_id'];
+  prepareTime: number;
+  active: boolean;
+  toppings: [string];
+  ingredients: [string];
+  isFeatured: boolean;
+  featuredExpiresAt: Date;
+  featuredStartsAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const FoodSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    img: { type: String, required: true },
+    rating: { type: Number, default: 0 },
+    description: { type: String, required: true },
+    size: { type: Types.Array, default: [] },
+    status: { type: String, enum: Object.values(Status), default: Status.IN_STOCK },
+    category: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
+      default: [],
+    },
+    prepareTime: { type: Number, default: 0 },
+    active: { type: Boolean, default: true },
+    toppings: { type: Types.Array, default: [] },
+    ingredients: { type: Types.Array, default: [] },
+    isFeatured: { type: Boolean, default: false },
+    featuredExpiresAt: { type: Date, default: null },
+    featuredStartsAt: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Food = mongoose.model<FoodDocument>('Food', FoodSchema);
+
+export default Food;
